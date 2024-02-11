cee40d5722ce0da568655db00c3655151ccaed61
CrossStack-Q <crossstack@gmail.com>
2024-02-11 22:37:35 +0530

    database setup done

--- backend/database/connect.go
+++ backend/database/connect.go
@@ -17,10 +17,14 @@ func Connect() {
 		log.Fatal(err)
 	}
 	dsn := os.Getenv("DSN")
+
+	// This is main command to connect to mysql
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to the database ")
+	} else {
+		log.Println("Connect successfully")
 	}
 
 	DB = db
